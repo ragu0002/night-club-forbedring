26f3fc3977e82da4861703066d9f55fb8c6ec38a
rosalina hundahl <rofr0001@stud.kea.dk>
2025-12-04 16:33:17 +0100

    rosa/add-static-gallery

---
+++ src/app/components/home/gallery/Gallery.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { HeadingMain } from "../../typography";
+import Image from "next/image";
+
+const images = [
+  "/assets/content-img/gallery1_big.jpg",
+  "/assets/content-img/gallery2_big.jpg",
+  "/assets/content-img/gallery3_big.jpg",
+  "/assets/content-img/gallery4_big.jpg",
+  "/assets/content-img/gallery5_big.jpg",
+  "/assets/content-img/gallery6_big.jpg",
+  "/assets/content-img/gallery7_big.jpg",
+];
+
+export default function Gallery() {
+  return (
+    <>
+      <section className="w-screen">
+        <div className="my-20">
+          <HeadingMain
+            color="white"
+            text="night club gallery"
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-4 *:object-cover *:self-stretch ">
+          {images.map((src, i) => (
+            <Image
+              key={i}
+              src={src}
+              width={1200}
+              height={800}
+              alt={`Gallery image ${i + 1}`}
+              className="w-full h-full object-cover"
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HomeHero from "./components/home/hero/HomeHero";
+import Gallery from "./components/home/gallery/Gallery";
 
 export default function Home() {
   return (
     <>
       <main className="grid col-(--full-col) grid-cols-subgrid">
         <HomeHero />
+        <Gallery />
       </main>
     </>
   );
