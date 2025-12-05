b6c4a0f301045e59b60f09b305d1f87d91e7db4c
rosalina hundahl <rofr0001@stud.kea.dk>
2025-12-05 13:04:14 +0100

    rosa/create-static-testimonials-component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import HomeHero from "./components/home/hero/HomeHero";
 import Events from "./components/home/events/Events";
 import Gallery from "./components/home/gallery/Gallery";
+import Testimonials from "./components/testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Events />
 
         <Gallery />
+        <Testimonials />
       </main>
     </>
   );
