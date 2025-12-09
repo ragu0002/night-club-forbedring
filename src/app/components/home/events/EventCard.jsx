76009dad80772d76c5aceaf8c0d24151fd8c46db
rosalina hundahl <rofr0001@stud.kea.dk>
2025-12-09 15:21:42 +0100

    rosa/add-event-card

--- src/app/components/home/events/EventCard.jsx
+++ src/app/components/home/events/EventCard.jsx
@@ -1,42 +1,47 @@
-import { BannerText } from "../../typography";
 import Image from "next/image";
+import { BannerText } from "../../typography";
+import HoverFrame from "../../hoverFrames/Triangles";
+import DetailsOverlay from "./DetailsOverlay";
 
-export default function eventCard({ imgUrl }) {
-  const eventImg = (
-    <>
-      <Image
-        src={`/assets/content-img/${imgUrl}`}
-        width={500}
-        height={500}
-        alt="Event image 1"
-        className="w-auto z-10 m"
-      />
-    </>
-  );
+export default function EventCard({ event }) {
+  const dateFormat = new Intl.DateTimeFormat("en-GB", {
+    month: "long",
+    day: "numeric",
+  });
 
-  const eventLabel = (
-    <>
-      <div className="w-auto  h-10 bg-accent flex items-center justify-start gap-7 px-6">
-        <BannerText
-          color="white"
-          text="DD./MM."
-        />
-        <BannerText
-          color="white"
-          text="00:00 PM"
-        />
-        <BannerText
-          color="white"
-          text="Location, City"
-        />
-      </div>
-    </>
-  );
+  const timeFormat = new Intl.DateTimeFormat("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  const date = dateFormat.format(new Date(event.date));
+  const time = timeFormat.format(new Date(event.date));
 
   return (
-    <div className="h-full w-full *:w-full ">
-      {eventImg}
-      {eventLabel}
+    <div className="relative w-full">
+      <HoverFrame>
+        <Image
+          src={`/assets/content-img/event-thumb${event.id}.jpg`}
+          alt={event.title || "Event image"}
+          width={403}
+          height={570}
+          className="w-full h-full object-cover"
+        />
+        <DetailsOverlay
+          key={event.id}
+          event={event}
+        />
+      </HoverFrame>
+
+      <div className="w-full h-10 bg-accent flex items-center justify-start gap-7 px-6">
+        <BannerText text={date} />
+        <BannerText
+          style="uppercase"
+          text={time}
+        />
+        <BannerText text={`${event.location}, New York`} />
+      </div>
     </div>
   );
 }
