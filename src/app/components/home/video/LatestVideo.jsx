87060649d26103ed00be74a13e4ce6eef5a1405b
Ragnhild <ragu0002@stud.kea.dk>
2025-12-13 15:24:12 +0100

    ragnhild-add-background

--- src/app/components/home/video/LatestVideo.jsx
+++ src/app/components/home/video/LatestVideo.jsx
@@ -1,31 +1,27 @@
-
 import { HeadingMain } from "../../typography";
 import VideoCard from "./VideoCard";
+import PatternBg from "../../bgOverlays/PatternBg";
 
 export default function LatestVideo() {
   return (
     <section className="col-(--full-col) grid grid-cols-subgrid py-20">
-      <div className="col-(--content-col) flex flex-col items-center gap-10">
-        <HeadingMain
-          color="white"
-          text="LATEST VIDEO"
-        />
+      <PatternBg>
+        <div className="col-(--content-col) flex flex-col items-center gap-10">
+          <HeadingMain color="white" text="LATEST VIDEO" />
 
-        <VideoCard
-          imgUrl="video_poster.jpg"
-          altText="videobillede"
-        />
+          <VideoCard imgUrl="video_poster.jpg" altText="videobillede" />
 
-        {/* Pile */}
-        <div className="flex items-center justify-center gap-4 mt-4">
-          <button className="w-10 h-10 border border-white flex items-center justify-center">
-            <span className="text-white text-lg">&lt;</span>
-          </button>
-          <button className="w-10 h-10 border border-white flex items-center justify-center">
-            <span className="text-white text-lg">&gt;</span>
-          </button>
+          {/* Pile */}
+          <div className="flex items-center justify-center gap-4 mt-4">
+            <button className="w-10 h-10 border border-white flex items-center justify-center">
+              <span className="text-white text-lg">&lt;</span>
+            </button>
+            <button className="w-10 h-10 border border-white flex items-center justify-center">
+              <span className="text-white text-lg">&gt;</span>
+            </button>
+          </div>
         </div>
-      </div>
+      </PatternBg>
     </section>
   );
 }
